f200eebf00321ca6e3572c4e71a0b7d5b66d80d4
Adrian Pearman <adrianpearman12@gmail.com>
2017-07-13 15:36:59 -0400

    changing between X and O functions added

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square(props) {
+function Square(props) {
   return(
     <button className = 'square' onClick = {props.onClick}>
       {props.value}
@@ -16,13 +16,17 @@ class Board extends React.Component{
     super();
     this.state = {
       squares: Array(9).fill(null),
+      xIsNext: true,
     }
   }
 
   handleClick(i){
     const squares = this.state.squares.slice();
-    squares[i] = 'X';
-    this.setState({squares: squares});
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
   }
 
   renderSquare(i){
